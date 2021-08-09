9156325897ef9acc7920bf5b8b66f294a1bdffc9
Nicholas Gamunorwa <nicholas.gamunorwa@cassavasmartech.co.zw>
2021-08-10 00:53:47 +0200

    first commit

---
+++ src/main/java/zw/co/pabuwe/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package zw.co.pabuwe.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    private String index(Model model){
+        return "index";
+    }
+}
